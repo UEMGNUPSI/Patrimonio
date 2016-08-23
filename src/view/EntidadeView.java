c3bf2407fcccaa7f9c46a697463d1169cddc7515
NUPSI-01 <NUPSI-01@NUPSI-01-PC>
2016-08-23 14:58:50 -0300

    Entidade View alterada, somente textos dos avisos.

--- src/view/EntidadeView.java
+++ src/view/EntidadeView.java
@@ -32,10 +32,11 @@ public class EntidadeView extends javax.swing.JInternalFrame {
     public EntidadeView() {
         entidadeDAO = new EntidadeDAO();
         listaEntidade = new ArrayList<>();
-
+        JOptionPane.showMessageDialog(null, "Gravado com Sucesso.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
         initComponents();  
         this.setVisible(true);
         atualizaTabelaEntidade();
+        
 
     }
 
