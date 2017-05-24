db95a065aa462f05634b32d656ddf91e6abda0a0
Danilo <Danilo@LAPTOP-009KQMVN>
2017-05-24 16:38:53 -0300

    Primeiro Commit

--- src/dao/BaixadoDAO.java
+++ src/dao/BaixadoDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.sql.PreparedStatement;
@@ -14,10 +9,7 @@ import model.BaixadoM;
 import model.BlocoM;
 import model.PatrimonioM;
 
-/**
- *
- * @author NUPSI-02
- */
+
 public class BaixadoDAO {
     PreparedStatement pst;
     String sql;
